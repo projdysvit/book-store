3dce9aa8c8a0166ea8929273364731b94599f61c
Riki <perekotypole@proton.me>
2024-02-27 16:02:33 +0200

    added dto validation and GlobalExceptionHandler

--- src/main/java/com/book/store/bookstore/dto/request/book/BookRequestDto.java
+++ src/main/java/com/book/store/bookstore/dto/request/book/BookRequestDto.java
@@ -1,13 +1,24 @@
 package com.book.store.bookstore.dto.request.book;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import lombok.Data;
 
-public record BookRequestDto(
-        String title,
-        String author,
-        String isbn,
-        BigDecimal price,
-        String description,
-        String coverImage
-) {
+@Data
+public class BookRequestDto {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String author;
+    @NotBlank
+    private String isbn;
+    @NotNull
+    @Min(0)
+    private BigDecimal price;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private String coverImage;
 }
---
+++ src/main/java/com/book/store/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.book.store.bookstore.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        body.put("errors", ex.getBindingResult().getAllErrors()
+                                .stream()
+                                .map(this::getErrorMessage)
+                                .toList());
+        return new ResponseEntity<>(body, headers, status);
+    }
+
+    private String getErrorMessage(ObjectError error) {
+        if (!(error instanceof FieldError)) {
+            return error.getDefaultMessage();
+        }
+
+        String field = ((FieldError) error).getField();
+        String message = error.getDefaultMessage();
+        return field + " " + message;
+    }
+}
