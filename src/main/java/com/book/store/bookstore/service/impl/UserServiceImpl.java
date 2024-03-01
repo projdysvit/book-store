f6062b2daaa81998f0dccaed0d29c0cc7d274d1c
Riki <perekotypole@proton.me>
2024-03-01 21:35:28 +0200

    spring boot security hw done

--- src/main/java/com/book/store/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/book/store/bookstore/service/impl/UserServiceImpl.java
@@ -2,18 +2,26 @@ package com.book.store.bookstore.service.impl;
 
 import com.book.store.bookstore.dto.request.user.UserRegistrationRequestDto;
 import com.book.store.bookstore.dto.response.user.UserResponseDto;
+import com.book.store.bookstore.exception.EntityNotFoundException;
 import com.book.store.bookstore.exception.RegistrationException;
 import com.book.store.bookstore.mapper.UserMapper;
+import com.book.store.bookstore.model.Role;
+import com.book.store.bookstore.model.User;
+import com.book.store.bookstore.repository.RoleRepository;
 import com.book.store.bookstore.repository.UserRepository;
 import com.book.store.bookstore.service.UserService;
+import java.util.Set;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserResponseDto create(UserRegistrationRequestDto requestDto) {
@@ -22,6 +30,16 @@ public class UserServiceImpl implements UserService {
                                             + requestDto.email());
         }
 
-        return userMapper.toDto(userRepository.save(userMapper.toModel(requestDto)));
+        User newUser = userMapper.toModel(requestDto);
+        newUser.setPassword(passwordEncoder.encode(requestDto.password()));
+        newUser.setRoles(Set.of(
+                roleRepository.findByName(Role.RoleName.ROLE_USER)
+                    .orElseThrow(
+                        () -> new EntityNotFoundException("Can't get arole.")
+                    )
+            )
+        );
+
+        return userMapper.toDto(userRepository.save(newUser));
     }
 }
