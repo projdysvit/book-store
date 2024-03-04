656980a85e12bf1ecb03fd00c7c8b9766ba8eecc
Riki <perekotypole@proton.me>
2024-03-04 15:51:35 +0200

    fixed

--- src/main/java/com/book/store/bookstore/dto/response/book/BookResponseDto.java
+++ src/main/java/com/book/store/bookstore/dto/response/book/BookResponseDto.java
@@ -1,7 +1,6 @@
 package com.book.store.bookstore.dto.response.book;
 
 import java.math.BigDecimal;
-import java.util.LinkedList;
 import java.util.List;
 
 public record BookResponseDto(
@@ -14,27 +13,4 @@ public record BookResponseDto(
         String coverImage,
         List<Long> categoryIds
 ) {
-    public BookResponseDto(
-            Long id,
-            String title,
-            String author,
-            String isbn,
-            BigDecimal price,
-            String description,
-            String coverImage,
-            List<Long> categoryIds
-    ) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.isbn = isbn;
-        this.price = price;
-        this.description = description;
-        this.coverImage = coverImage;
-        this.categoryIds = new LinkedList<>();
-    }
-
-    public void setCategoryIds(List<Long> categoryIds) {
-        this.categoryIds.addAll(categoryIds);
-    }
 }
--- src/main/java/com/book/store/bookstore/mapper/BookMapper.java
+++ src/main/java/com/book/store/bookstore/mapper/BookMapper.java
@@ -5,15 +5,22 @@ import com.book.store.bookstore.dto.request.book.BookRequestDto;
 import com.book.store.bookstore.dto.response.book.BookResponseDto;
 import com.book.store.bookstore.dto.response.book.BookWithoutCategoriesResponseDto;
 import com.book.store.bookstore.model.Book;
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
 import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface BookMapper {
+    @Mapping(
+            target = "categoryIds",
+            expression = "java("
+                + "book.getCategories()"
+                    + ".stream()"
+                    + ".map(category -> category.getId())"
+                    + ".toList()"
+            + ")"
+    )
     BookResponseDto toDto(Book book);
 
     BookWithoutCategoriesResponseDto toDtoWithoutCategories(Book book);
@@ -23,16 +30,6 @@ public interface BookMapper {
             @Mapping(target = "isDeleted", ignore = true)
     })
     Book toModel(BookRequestDto bookDto);
-
-    @AfterMapping
-    default void setCategories(@MappingTarget BookResponseDto bookDto, Book book) {
-        bookDto.setCategoryIds(
-                book.getCategories()
-                    .stream()
-                    .map(category -> category.getId())
-                    .toList()
-        );
-    }
 
     @Named("bookFromId")
     default Book bookFromId(Long id) {
